9deef39923824cf734efc67600fe1dffa6f40f75
Hatem <hatem.deux@gmail.com>
2020-02-04 20:54:04 -0500

    code rechecked and enhanced, missed test cases, etc

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,5 @@
 const eqArrays = function(leftArray, rightArray){
+  if ( leftArray.length !== rightArray.length) return false;
   for (let i = 0; i < leftArray.length; i++){
     if ( leftArray[i] !== rightArray[i]) return false;
   }
@@ -22,3 +23,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+assertEqual(eqArrays([1, 2, 3, 4], [3, 2, 1]), false);
+assertEqual(eqArrays([], [3, 2, 1]), false);
+
