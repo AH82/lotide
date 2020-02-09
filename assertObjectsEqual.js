3915c38c626e228df4411dd2a51cab43e1fad1b4
Hatem <hatem.deux@gmail.com>
2020-02-09 12:08:40 -0500

    Comments added describing misjudge on the function that this one is based on and doing necessary code to reflect

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -35,14 +35,30 @@ const eqObjects = function(object1, object2) {
 };
 
 // ACTUAL FUNCTION
+/**
+ * IMPORTANT NOTE:
+ * In my original implementation of assertArrayEqual() - the function on which this one is based, due misorientation/misunderstanding of instruction,
+ * I have "invented" a 3rd parameter called "expectedBool" in which you insert the the boolian you are expecting.
+ * assertArraysEqual = function(leftArray, RightArray, expectedBool) will return true/false and insert it in a corresponding message for visual check.
+ * this time though, I'm following instruction by the walkthrough, though will leave the equivalent implementation of assertArraysEqual() commented out.
+ * an extra EoL comment //#EOIoAAE will be added to those statements. 
+ */
 const assertObjectsEqual = function(actual, expected) {
+  // importing util library's inspect function, as per walkthrough instruction.
+  const inspect = require('util').inspect; 
+
+  // my implementation >>
   if (eqObjects(actual, expected) === true) {
-      console.log(`✅✅✅ Object Assertion Passed: === TRUE.`);
-      // return true;
-    } else {
-      console.log(`🛑🛑🛑 Object Assertion Failed: === False (!== true).`);
-      // return false;
+    // console.log(`✅✅✅ Object Assertion Passed: === TRUE.`);                  //#EOIoAAE
+    // return true;                                                                //#EOIoAAE
+    console.log(`✅✅✅ object ${actual} === object ${expected}`);
+  } else { 
+    // console.log(`🛑🛑🛑 Object Assertion Failed: === False (!== true).`);      //#EOIoAAE
+    // return false;                                                                //#EOIoAAE
+    console.log(`🛑🛑🛑 object ${actual} !== object ${expected}`);
     }
   
 };
 // TEST ASSERTION CODE
+assertObjectsEqual({a:1, b:2, c:3}, {a:1, b:2, c:3});
+assertObjectsEqual({a:1, b:2, c:3}, {a:1, b:2, c:4});
