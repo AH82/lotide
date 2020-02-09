181b45a3c6f37015a234049cc4ad2db21126ddbd
Hatem <hatem.deux@gmail.com>
2020-02-09 12:26:31 -0500

    linting and cosmetic edits

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -41,22 +41,22 @@ const eqObjects = function(object1, object2) {
  * I have "invented" a 3rd parameter called "expectedBool" in which you insert the the boolian you are expecting.
  * assertArraysEqual = function(leftArray, RightArray, expectedBool) will return true/false and insert it in a corresponding message for visual check.
  * this time though, I'm following instruction by the walkthrough, though will leave the equivalent implementation of assertArraysEqual() commented out.
- * an extra EoL comment //#EOIoAAE will be added to those statements. 
+ * an extra EoL comment //#EOIoAAE will be added to those statements.
  */
 const assertObjectsEqual = function(actual, expected) {
   // importing util library's inspect function, as per walkthrough instruction.
-  const inspect = require('util').inspect; 
+  const inspect = require('util').inspect;
 
   // my implementation >>
   if (eqObjects(actual, expected) === true) {
     // console.log(`✅✅✅ Object Assertion Passed: === TRUE.`);                  //#EOIoAAE
     // return true;                                                                //#EOIoAAE
     console.log(`✅✅✅ object ${inspect(actual)} === object ${inspect(expected)}`);
-  } else { 
+  } else {
     // console.log(`🛑🛑🛑 Object Assertion Failed: === False (!== true).`);      //#EOIoAAE
     // return false;                                                                //#EOIoAAE
     console.log(`🛑🛑🛑 object ${inspect(actual)} !== object ${inspect(expected)}`);
-    }
+  }
   
   
 };
