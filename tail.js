b48add21fb52fd187a3687c87956efbed0aa5aab
Hatem <hatem.deux@gmail.com>
2020-02-10 14:04:22 -0500

    tail to( tail + tail test) implementation

--- tail.js
+++ tail.js
@@ -1,9 +1,9 @@
-//assertion code
+/* //assertion code
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}.`);
   } else  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`);
-};
+}; */
 
 //the actual code
 
@@ -14,12 +14,16 @@ const tail = function(anArray) {
   }
   return tailArray;
 };
-console.log(tail([5,6,4,3,8]));
 
-//test cases
+
+
+/* //test cases
 // assertEqual(tail([5,6,7]), [6,7]); //returns falsy false
+console.log(tail([5,6,4,3,8]));
 // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs" */
+
+module.exports = tail;
