ea99ef01bcfe15587d4b926f8c083dbc88ac0245
Hatem <hatem.deux@gmail.com>
2020-02-10 18:51:02 -0500

    Mocha and chai applied to JSs head, middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,64 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
+
+describe("#middle", () => {
+  // TESTING RETURN OF EMPTY STRING.
+  it("returns [] for []", ()=> {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [] for [1]", ()=> {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [5, 6]", ()=> {
+    assert.deepEqual(middle([5, 6]), []);
+  });
+  it("[1, 2, 3] doesn't return []", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3]), []);
+  });
+  it("[1, 2, 3, 4] doesn't return []", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), []);
+  });
+
+  //TESTING RETURN OF ODD NUMBERED ARRAYS
+  it("ODD array of [1] doesn't return [1]", ()=> {
+    assert.notDeepEqual(middle([1]), [1]);
+  });
+  it("ODD array of [1, 2, 3] return [2]", ()=> {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("ODD array of [1, 2, 3, 4 ,5] return [3]", ()=> {
+    assert.deepEqual(middle([1, 2, 3, 4 ,5]), [3]);
+  });
+// TESTING RETURN OF EVEN NUMBERED ARRAYS
+  it("EVEN array of [1, 2] do not return [1]", ()=> {
+    assert.notDeepEqual(middle([1, 2]), [1]);
+  });
+  it("EVEN array of [1, 2] do not return [2]", ()=> {
+    assert.notDeepEqual(middle([1, 2]), [2]);
+  });
+  it("EVEN array of [1, 2, 3, 4] returns [2, 3]", ()=> {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+  it("EVEN array of [1, 2, 3, 4] do not return [1, 2]", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [1, 2]);
+  });
+  it("EVEN array of [1, 2, 3, 4] do not return [3, 4]", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [3, 4]);
+  });
+  it("EVEN array of [1, 2, 3, 4] do not return [2]", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [2]);
+  });  it("EVEN array of [1, 2, 3, 4] do not return [3]", ()=> {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [3]);
+  });
+});
+
+// ------------------------------------------------------
+// THE FOLLOWING IS OLD CODE 
+// THAT SHOULD BE DELETED AS PER "COMPASS" INSTRUCTIONS.
+// I'M KEEPING THE CODE FOR LEARNING PURPOSES.
+// ------------------------------------------------------
+
+/* const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
 // TEST ASSERTION CODE
@@ -32,3 +92,4 @@ assertArraysEqual(middle([1,2,3,4,5,6]),[2,4],false);
 assertArraysEqual(middle([1,2,3,4,5,6]),[4,5],false);
 
 
+ */
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,32 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
+
+describe("#tail", () => {
+  it("Array [5,6,7] returns [6,7] ", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+  it("Array 'Hello', 'Lighthouse', 'Labs']) -> returns ['Lighthouse', 'Labs'] ", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("Array [5,6,7] does not return [5, 6, 7] ", () => {
+    assert.notDeepEqual(tail([5, 6, 7]), [5, 6, 7]);
+  });  it("Array [4,5,6,7] does not returns [6,7] ", () => {
+    assert.notDeepEqual(tail([4, 5, 6, 7]), [6, 7]);
+  });
+});
+
+
+
+
+
+// ------------------------------------------------------
+// THE FOLLOWING IS OLD CODE 
+// THAT SHOULD BE DELETED AS PER "COMPASS" INSTRUCTIONS.
+// I'M KEEPING THE CODE FOR LEARNING PURPOSES.
+// ------------------------------------------------------
+
+
+/* const tail = require('../tail');
 const assertEqual = require('../assertEqual')
 
 //test cases
@@ -8,4 +36,4 @@ console.log(tail([5,6,4,3,8]));
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs" */
