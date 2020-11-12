c053af4da0a441772fb02ccf3fd88c6ee9941c27
Hatem <hatem.deux@gmail.com>
2020-11-11 20:42:04 -0500

    refactor: use of Array.isArray() instead of typeof === 'object'

--- flatten.js
+++ flatten.js
@@ -5,7 +5,7 @@ const flatten = function(sourceArray) {
   // return flattened array of any nesting level (but implement just for 1)
   let tempArray = [];
   for (let i = 0; i < sourceArray.length; i++) {
-    if (typeof(sourceArray[i]) === "object") {
+    if (Array.isArray(sourceArray[i])) {
       for (let j = 0; j < sourceArray[i].length; j++) {
         // push into new array
         tempArray.push(sourceArray[i][j]);
