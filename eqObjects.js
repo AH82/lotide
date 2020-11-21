ff3364fa80c6a8c37e9fa35e6837a7b6759a9430
Hatem <hatem.deux@gmail.com>
2020-11-20 19:57:40 -0500

    implements deep object comparison through recursion + test cases

--- eqObjects.js
+++ eqObjects.js
@@ -15,10 +15,12 @@ const eqArrays = function(leftArray, rightArray) {
 
 
 // ACTUAL FUNCTION
-
 const eqObjects = function(object1, object2) {
+  // uncomment the following line to test Recursion happening
+  // console.log("object are : \n", object1, "&& \n",object2)
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
+
   } else {
     for (let arrKey of Object.keys(object1)) {
       
@@ -26,8 +28,12 @@ const eqObjects = function(object1, object2) {
         if (!eqArrays(object1[arrKey], object2[arrKey])) {
           return false;
         }
+      } else if (typeof object1[arrKey] === 'object' && typeof object2[arrKey] === 'object') { 
+        if (!eqObjects(object1[arrKey], object2[arrKey])) return false;
+
       } else if (object1[arrKey] !== object2[arrKey]) {
         return false;
+
       }
     }
     return true;
@@ -56,3 +62,14 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, cd2), false);
 assertEqual(eqArrays(cd.d, cd2.d), false);
+
+// TEST CASES for RECURSION 
+console.log()
+console.log("TEST CASES for RECURSION");
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true,);
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 3 }), false,);
+assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: 5, n: 6 }}, { a: { z: 1 }, b: 2, c: {m: 5, n: 6} }), true,);
+assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9] }, d: 10}, { a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9]}, d: 10 }), true,); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); 
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
