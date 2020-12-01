3c81234be62a22cab6ea0d88b889a3fe39390177
Hatem <hatem.deux@gmail.com>
2020-12-01 03:24:21 -0500

    converts test cases to proper mocha/chai test cases. comments-out old custom assertions (kept for learning purposes

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,30 +1,140 @@
+const assert = require("chai").assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
 describe("#eqArrays", () => {
-  describe("Shallow Comparison", () => {
-    it("Using #assertEqual", () => {
-      //Test cases
-      assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-      assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-      assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-      assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-      assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
-      assertEqual(eqArrays([1, 2, 3, 4], [3, 2, 1]), false);
-      assertEqual(eqArrays([], [3, 2, 1]), false);
+  
+  describe("returns TRUE when", () => {
+    it("arrays of numbers are identical", () => {
+      assert.isTrue(
+        eqArrays([1, 2, 3], [1, 2, 3])
+      );
+    });
+
+    it("arrays of strings are identical", () => {
+      assert.isTrue(
+        eqArrays(["1", "2", "3"], ["1", "2", "3"])
+      );
+    });
+
+  });
+
+  describe("returns FALSE when", () => {
+
+    it("arrays' elements are strict equal (not coersed)", () => {
+      assert.isFalse(
+        eqArrays(["1", "2", "3"], ["1", "2", 3])
+      );
+    });
+
+    it("arrays' length are not equals", () => {
+      assert.isFalse(
+        eqArrays(["1", "2", "3", "4"], ["1", "2", "3"])
+        
+      );
+      assert.isFalse(
+        eqArrays([1, 2, 3, 4], [3, 2, 1])
+      );
+    });
+
+    it("one of the arrays is empty", () => {
+      assert.isFalse(
+        eqArrays([], [3, 2, 1])
+      );
+      assert.isFalse(
+        eqArrays([3, 2, 1], [])
+      );
+    });
+
+  });
+
+  describe("Deep Comparison (recursion)", () => {
+    
+    describe("returns TRUE when", () => {
+      
+      it("all arrays & their nested arrays are equal", () => {
+        assert.isTrue(
+          eqArrays(
+            [[2, 3], [4]],
+            [[2, 3], [4]]
+          )
+        );
+        assert.isTrue(
+          eqArrays(
+            [[2, 3], [4], 8],
+            [[2, 3], [4], 8]
+          )
+        );
+        assert.isTrue(
+          eqArrays(
+            [[2, [[[3]], 9]], [4], 8],
+            [[2, [[[3]], 9]], [4], 8]
+          )
+        );
+      });
 
     });
-  });
   
-  describe("Deep Comparison with recursion", () => {
-    it("Using #assertEqual", () => {
-      // TEST CASES : Recursion / deep comparison 
-      assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-      assertEqual(eqArrays([[2, 3], [4], 8], [[2, 3], [4], 8]), true);
-      assertEqual(eqArrays([[2, 3], [4], 8], [[2, 3], [4], 9]), false);
-      assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-      assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-    })
-    
+    describe("returns FALSE when", () => {
+
+      it("nested array of same length are not equal", () => {
+        assert.isFalse(
+          eqArrays(
+            [[2, 3], [4], 8],
+            [[2, 3], [5], 8]
+          )
+        );
+      });
+
+      it("nested arrays are not of equal length to otherwise identical arrays", () => {
+        assert.isFalse(
+          eqArrays(
+            [[2, 3], [4]],
+            [[2, 3], [4, 4]]
+          )
+        );
+        assert.isFalse(
+          eqArrays(
+            [[2, 3], [4, 4]],
+            [[2, 3], [4]]
+          )
+        );
+      });
+
+      it("element is not equal to a nested array with same element value", () => {
+        assert.isFalse(
+          eqArrays(
+            [[2, 3], [4]],
+            [[2, 3], 4]
+          )
+        );
+      });
+
+    });
+
   });
+
 });
+
+
+/*
+// OLD CUSTOM TEST CASES
+
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+assertEqual(eqArrays([1, 2, 3, 4], [3, 2, 1]), false);
+assertEqual(eqArrays([], [3, 2, 1]), false);
+
+  // TEST CASES : Recursion / deep comparison
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  assertEqual(eqArrays([[2, 3], [4], 8], [[2, 3], [4], 8]), true);
+  assertEqual(eqArrays([[2, 3], [4], 8], [[2, 3], [4], 9]), false);
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+
+*/
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,39 +1,147 @@
+const assert = require("chai").assert;
 const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
 const eqObjects = require("../eqObjects");
 
 
-// TEST ASSERTION CODE
+describe('#eqObjects', () => {
 
+  // TEST DATA 1
+  const ab = { a: "1", b: "2" };
+  const ab3 = { a: "1", b: "3" };
+  const ba = { b: "2", a: "1" };
+  const abc = { a: "1", b: "2", c: "3" };
+  
+  it(`returns true when objects are identical`, () => {
+    assert.deepEqual(ab, ba);
+    assert.isTrue(eqObjects(ab, ba));
+  });
+  
+  it("returns false when numbers of keys are not equal", () => {
+    assert.notDeepEqual(ab, abc);
+    assert.isFalse(eqObjects(ab, abc));
+  });
+  
+  it("returns false when identical keys has different values", () => {
+    assert.notDeepEqual(ab, ab3);
+    assert.isFalse(eqObjects(ab, ab3));
+  });
+  
+  const cd = { c: "1", d: ["2", 3] };
+  const dc = { d: ["2", 3], c: "1" };
+  const cd2 = { c: "1", d: ["2", 3, 4] };
+  
+  it(`returns true on equal objects with equal nested arrays`, () => {
+    assert.deepEqual(cd,dc);
+    assert.isTrue(eqObjects(cd, cd));
+  });
+  
+  it(`returns false on objects with different nested arrays to equal keys, and equal other props`, () => {
+    assert.notDeepEqual(cd, cd2);
+    assert.isFalse(eqObjects(cd, cd2));
+  });
+  
+  describe("deep comparison (recursion)", () => {
+    
+    // TEST DATA 2
+    const ax1b2 = { a: { x: 1 }, b: 2 };
+    const ax2b2 = { a: { x: 2 }, b: 2 };
+    const ay1b2 = { a: { y: 1 }, b: 2 };
+    const ax7y1b2 = { a: { x:7, y: 1 }, b: 2 };
+    
+    const ax_A34_b2 = { a: { x: [ 3, 4 ] }, b: 2 };
+    const ax_A35_b2 = { a: { x: [ 3, 5 ] }, b: 2 };
+    
+    describe("should return TRUE when", () => {
+      
+      it("equal nested objects", () => {
+        assert.deepEqual(ax1b2, { a: { x: 1 }, b: 2 });
+        assert.isTrue(eqObjects(ax1b2, { a: { x: 1 }, b: 2 }));
+      });
+      
+      it("equal nested objects containing equal arrays values", () => {
+        assert.deepEqual(ax_A34_b2, { a: { x: [ 3, 4 ] }, b: 2 });
+        assert.isTrue(eqObjects(ax_A34_b2, { a: { x: [ 3, 4 ] }, b: 2 }));
+      });
+      
+    });
+    
+    describe("should return FALSE when", () => {
+      
+      it("nested identical keys have different values", () => {
+        assert.notDeepEqual(ax1b2, ax2b2);
+        assert.isFalse(eqObjects(ax1b2, ax2b2));
+      });
+      
+      it("nested object keys are NOT equal", () => {
+        assert.notDeepEqual(ax1b2, ay1b2);
+        assert.isFalse(eqObjects(ax1b2, ay1b2));
+      });
+      
+      it("nested object number of keys are NOT equal", () => {
+        assert.notDeepEqual(ax7y1b2, ay1b2);
+        assert.isFalse(eqObjects(ax7y1b2, ay1b2));
+      });
+      
+      it("equal nested objects containing NOT equal arrays values", () => {
+        assert.notDeepEqual(ax1b2, ax_A34_b2);
+        assert.isFalse(eqObjects(ax1b2, ax_A34_b2));
+      });
+      
+      it("nested object vs. flat object value", () => {
+        assert.notDeepEqual(ax_A34_b2, ax_A35_b2);
+        assert.isFalse(eqObjects(ax_A34_b2, ax_A35_b2));
+      });
+      
+    });
+    describe("All Combinations", () => {
+      it("returns true of muti-nested and multi type object", () => {
+        assert.deepEqual(
+          { a: { z: 1 }, b: 2, c: { m: { e:11, f: 22, g: 33}, n: [8, 9] }, d: 10 },
+          { a: { z: 1 }, b: 2, c: { m: { e:11, f: 22, g: 33}, n: [8, 9] }, d: 10 }
+        );
+        assert.isTrue(
+          eqObjects(
+            { a: { z: 1 }, b: 2, c: { m: {e:11, f: 22, g: 33}, n: [8, 9] }, d: 10 },
+            { a: { z: 1 }, b: 2, c: { m: {e:11, f: 22, g: 33}, n: [8, 9] }, d: 10 }
+          )
+        );
+      });
+    });
+    
+    
+  });
+  
+  
+});
+  
+/*
+  // TEST ASSERTION CODE
+  const eqArrays = require("../eqArrays");
+  const ab = { a: "1", b: "2" };
+  const ba = { b: "2", a: "1" };
+  assertEqual(eqObjects(ab, ba), true);
+  
+  const abc = { a: "1", b: "2", c: "3" };
+  assertEqual(eqObjects(ab, abc), false);
+  
+  
+  // TEST ASSERTION CODE -- PART-2
+  const cd = { c: "1", d: ["2", 3] };
+  const dc = { d: ["2", 3], c: "1" };
+  assertEqual(eqObjects(cd, dc), true);
+  assertEqual(eqArrays(cd.d,dc.d), true);
+  
+  const cd2 = { c: "1", d: ["2", 3, 4] };
+  
+  assertEqual(eqObjects(cd, cd2), false);
+  assertEqual(eqArrays(cd.d, cd2.d), false);
 
+  // TEST CASES for RECURSION
+  assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true,);
+  assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 3 }), false,);
+  assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: 5, n: 6 }}, { a: { z: 1 }, b: 2, c: {m: 5, n: 6} }), true,);
+  assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9] }, d: 10}, { a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9]}, d: 10 }), true,); // => true
+  assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-
-// TEST ASSERTION CODE -- PART-2
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqArrays(cd.d,dc.d), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-
-assertEqual(eqObjects(cd, cd2), false);
-assertEqual(eqArrays(cd.d, cd2.d), false);
-
-// TEST CASES for RECURSION 
-console.log()
-console.log("TEST CASES for RECURSION");
-
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true,);
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 3 }), false,);
-assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: 5, n: 6 }}, { a: { z: 1 }, b: 2, c: {m: 5, n: 6} }), true,);
-assertEqual(eqObjects({ a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9] }, d: 10}, { a: { z: 1 }, b: 2, c: {m: {e:11, f: 22, g: 33}, n: [8, 9]}, d: 10 }), true,); // => true
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); 
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+*/
